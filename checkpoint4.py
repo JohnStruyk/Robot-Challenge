59608ab4458a11033afc7755397f6863fad5b70d
Riley Donat <riley.donat@yahoo.com>
2026-03-22 20:23:36 -0500

    ad

--- checkpoint4.py
+++ checkpoint4.py
@@ -3,16 +3,17 @@ from checkpoint0 import get_transform_camera_robot
 
 import cv2, time
 import numpy as np
-from xarm.wrapper import XArmAPI
 
+from xarm.wrapper import XArmAPI
 from utils.vis_utils import draw_pose_axes
 from utils.zed_camera import ZedCamera
 from checkpoint1 import grasp_cube, place_cube, GRIPPER_LENGTH, robot_ip
 
-# TODO: Determine a suitable height yourself
-# The cube is 0.025m. We need to clear the green cube then drop.
+# TODO:
+# Determine cube size
+
 CUBE_SIZE = 0.020
-STACK_HEIGHT = CUBE_SIZE # + 0.005 
+STACK_HEIGHT = CUBE_SIZE # + 0.005
 
 def main():
 
@@ -34,41 +35,45 @@ def main():
     time.sleep(0.5)
 
     try:
+
         # Get Observation
         cv_image = zed.image
 
-        # 1. Establish Camera-to-Robot Transform
+        # Establish Camera-to-Robot Transform
         t_cam_robot = get_transform_camera_robot(cv_image, camera_intrinsic)
+
         if t_cam_robot is None:
+
             print("Failed to find Camera-to-Robot transform.")
             return
+
         cube_pose_detector.set_camera_pose(t_cam_robot)
 
-
         # TODO
-        # Locate the RED cube (The one we pick up)
+        # Locate the RED cube 
         res_red = cube_pose_detector.get_transforms(cv_image, "red cube")
-        
-        # Locate the GREEN cube (The one we stack on)
+
+        # Locate the GREEN cube 
         res_green = cube_pose_detector.get_transforms(cv_image, "green cube")
 
-
         if res_red is None or res_green is None:
+
             print("Could not find both cubes.")
             return
-        
+
         t_robot_red, t_cam_red = res_red
         t_robot_green, t_cam_green = res_green
 
-        # Draw axes for both to be verify
+        # Draw axes for both to verify
         draw_pose_axes(cv_image, camera_intrinsic, t_cam_red)
         draw_pose_axes(cv_image, camera_intrinsic, t_cam_green)
-        
+
         cv2.imshow('Verifying Cube Poses', cv_image)
         key = cv2.waitKey(0)
 
         # TODO: Sequence the robot's actions
         if key == ord('k'):
+
             cv2.destroyAllWindows()
 
             # --- PICK UP RED ---
@@ -78,14 +83,12 @@ def main():
             # --- PREPARE STACK POSE ---
             # We take the green cube's location and add our STACK_HEIGHT to the Z axis
             t_robot_stack = np.copy(t_robot_green)
-            t_robot_stack[2, 3] += STACK_HEIGHT 
+            t_robot_stack[2, 3] += STACK_HEIGHT
 
             # --- PLACE ON GREEN ---
             print("Stacking on GREEN cube...")
             place_cube(arm, t_robot_stack)
-            
-            print("Task Complete.")
-    
+
     finally:
         # Close Lite6 Robot
         arm.move_gohome(wait=True)
